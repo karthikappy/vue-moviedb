2d5d484f9a722681c7565bd3638fd0483fb88720
Karthik Sankaran <contact@karthiksankaran.com>
2019-03-09 11:35:22 -0500

    Fixed bootstrap implementation

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 require('dotenv').config()
+//require("bootstrap")
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 new Vue({
   render: h => h(App),
