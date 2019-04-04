bca6e9c6f43fbb6f700927aa077b7f943de497ec
Karthik Sankaran <contact@karthiksankaran.com>
2019-04-04 07:35:32 -0400

    Implemented FontAwesome

--- src/main.js
+++ src/main.js
@@ -8,6 +8,18 @@ require('dotenv').config()
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+library.add(faChevronDown)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
